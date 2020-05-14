8f70326f53f40d46f8e731bf93cab2424edc1fe2
胡旭辉 <xuhui.hu@weimob.com>
2020-05-14 09:52:41 +0800

    添加json-server

---
+++ user.js
@@ -0,0 +1,17 @@
+//customers.js
+const faker = require('faker')
+
+const User = ()=>{
+  return {
+    user: {
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      website: faker.internet.url(),
+      address: faker.address.streetAddress() + faker.address.city() + faker.address.country(),
+      bio: faker.lorem.sentences(),
+      image: faker.image.avatar()
+    }
+  }
+}
+
+module.exports = User
